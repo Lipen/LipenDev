06d366054dbcfa6b35236a7797d849e1ac743c5b
Lipen <lipen00@gmail.com>
2015-03-10 21:36:34 +0300

    Constants now looks like constants. +minor enchantments

--- shipsim.py
+++ shipsim.py
@@ -1,24 +1,31 @@
 from tkinter import *
 import random
 
-title = 'shipsim'
-w = 800
-h = 600
+_TITLE = 'shipsim'
+_WIDTH = 800
+_HEIGHT = 600
 _FPS = 50.
 _DELAY = round(1000. / _FPS)
 _FPS = 1000./_DELAY
 print('FPS = {}, DELAY = {}'.format(_FPS, _DELAY))
 
 root = Tk()
-root.title(title)
-canv = Canvas(root, width=w, height=h, bg='#505050')
+root.title(_TITLE)
+canv = Canvas(root, width=_WIDTH, height=_HEIGHT, bg='#505050')
+
+
+def getRandomColor():
+	return '#{:06X}'.format(random.randint(0, 16777215))
 
 
 # class Vector2:
-# eh
 
 
 class Point:
+
+	'''
+	Base 2D point class.
+	'''
 
 	def __init__(self, x, y):
 		self.x, self.y = x, y
@@ -32,10 +39,16 @@ class Point:
 
 
 class Polygon:
+
+	'''
+	Base 2D polygon class.
+	* 4-point for now.
+	'''
+
 	vx = 0
 	vy = 0
 
-	def __init__(self, p1, p2, p3, p4, fill='#5060BB', activefill='#A060BB'):
+	def __init__(self, p1, p2, p3, p4, fill='#5060BB', activefill='#99dd33'):
 		self.points = [p1, p2, p3, p4]
 		self.p1, self.p2, self.p3, self.p4 = self.points
 		self.poly = canv.create_polygon(self.getCoords(), fill=fill, activefill=activefill)
@@ -67,11 +80,11 @@ class Polygon:
 		canv.coords(self.poly, self.p1.x, self.p1.y, self.p2.x, self.p2.y, self.p3.x, self.p3.y, self.p4.x, self.p4.y)
 
 	def newRandomPolygon():
-		p1 = Point(random.uniform(0, w), random.uniform(0, h))
-		p2 = Point(random.uniform(0, w), random.uniform(p1.y, h))
-		p3 = Point(random.uniform(max(p1.x, p2.x), w), random.uniform(0, h))
-		p4 = Point(random.uniform(max(p1.x, p2.x), w), random.uniform(p3.y, h))
-		return Polygon(p1, p2, p3, p4, fill='#{:X}{:X}{:X}'.format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+		p1 = Point(random.uniform(0, _WIDTH), random.uniform(0, _HEIGHT))
+		p2 = Point(random.uniform(0, _WIDTH), random.uniform(p1.y, _HEIGHT))
+		p3 = Point(random.uniform(max(p1.x, p2.x), _WIDTH), random.uniform(0, _HEIGHT))
+		p4 = Point(random.uniform(max(p1.x, p2.x), _WIDTH), random.uniform(p3.y, _HEIGHT))
+		return Polygon(p1, p2, p3, p4, fill=getRandomColor())
 
 
 def tick(event=None):
@@ -84,12 +97,20 @@ def timer():
 	#
 	root.after(_DELAY, timer)  # repeat
 
+
+def QuitDestroy(event=None):
+	print('<Q> pressed.')
+	root.destroy()
+	root.quit()
+	print('Destroyed.')
+
 ###
-mypoly = Polygon.newRandomPolygon()
+# mypoly = Polygon.newRandomPolygon()
+mypoly = Polygon(Point(100, 400), Point(80, 250), Point(400, 270), Point(300, 390), fill=getRandomColor())
 mypoly.setSpeed(1, 1)
 root.bind('z', tick)
+root.bind('q', QuitDestroy)
 root.after(_DELAY, timer)  # start
-# Polygon.getCoords()  # eh???
 ###
 
 canv.pack()
