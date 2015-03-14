5c52baea7691e1e6f3361911b2302814452f404d
Lipen <lipen00@gmail.com>
2015-03-14 22:56:34 +0300

    Implemented Cell

--- testField.py
+++ testField.py
@@ -1,23 +1,28 @@
 from tkinter import *
 from common import *
+from Vector2 import Vector2
+from Point import Point
 
 _TITLE = 'nxtsim'
-_WIDTH = 800
-_HEIGHT = 608
-_FPS = 25.
-_DELAY = 1./_FPS  # seconds
+_WIDTH = 768
+_HEIGHT = 640
+_FPS = 25
+_DELAY = 1/_FPS  # seconds
 _STARTTIME = time.time()
-
-root = Tk()
-root.title(_TITLE)
-canv = Canvas(root, width=_WIDTH, height=_HEIGHT, bg='#505050')
-label_fps = Label(root, bg='#cccccc', fg='#000000', font='sans 20')
 
 
 class Cell:
 
-	def __init__(self, x, y, width, height):
+	def __init__(self, x, y, width, height, color=None, activecolor='#00FF33'):
+		if color is None:
+			color = getRandomColor()
 		self.pos = Point(x, y)
+		self.width, self.height = width, height
+		self.label = Label(root, width=width, height=height, background=color, activebackground=activecolor)
+		self.label.place(x=x, y=y)
+
+	def isContainsPoint(self, p):
+		return self.pos.x <= p.x <= self.pos.x+self.width and self.pos.y <= p.y <= self.pos.y+self.height
 
 
 class Field:
@@ -25,9 +30,14 @@ class Field:
 	"""Field class for canvas with grid
 	"""
 
-	def __init__(self, n, m, cellx, celly):
+	def __init__(self, n, m, cellw, cellh):
 		self.n, self.m = n, m
-		self.cellx, self.celly = cellx, celly
+		self.cells = [[Cell(i*cellw, j*cellh, cellw, cellh, '#999999') for j in range(m)] for i in range(n)]
+
+	def getCellFromPoint(self, p):
+		for cell in self.cells:
+			if cell.isContainsPoint(p):
+				return cell
 
 	def update(self):
 		pass
@@ -44,19 +54,25 @@ def timer():
 	root.after((lambda t: t if t > 0 else 1)(round(1000*_DELAY)), timer)
 
 
-def QuitDestroy(event=None):
-	root.destroy()
+def Quit(event=None):
 	root.quit()
-	print('Destroyed!')
+	print('Quit!')
 
-field = Field(25, 19, 32, 32)
+root = Tk()
+root.title(_TITLE)
+root.geometry('{}x{}+420+60'.format(_WIDTH, _HEIGHT))
+root.protocol('WM_DELETE_WINDOW', Quit)
+root.resizable(False, False)
+canv = Canvas(root, width=_WIDTH, height=_HEIGHT, bg='#333333')
+canv.pack()
 clock = clock_yield()
 next(clock)  # get out initial zero
-
-root.bind('<Escape>', QuitDestroy)
-root.bind('<Control-c>', QuitDestroy)
-
-canv.pack()
+field = Field(6, 5, 128, 128)
+label_fps = Label(root, bg='#dddddd', fg='#000000', font='sans 20')
 label_fps.place(x=16, y=16, width=64, height=64)
+
+root.bind('<Escape>', Quit)
+root.bind('<Control-c>', Quit)
+
 root.after((lambda t: t if t > 0 else 1)(round(1000*_DELAY)), timer)
 root.mainloop()
