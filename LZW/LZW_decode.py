6f7ca8e7c129aafff607b73ec8c5f31de99a7507
Lipen <lipen00@gmail.com>
2015-03-26 21:53:11 +0300

    LZW Initial commit

---
+++ LZW/LZW_decode.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+f = open('LZW_Encoded.txt', 'r')
+s = f.read().splitlines()
+f.close()
+print('File contains:\n-----\n{}\n-----'.format(s))
+d = [chr(i) for i in range(256)]
+q = 256
+
+stack = ''
+flow = []
+
+for i in range(len(s)):
+	c = d[int(s[i])]
+	stack += c
+	if stack in d:
+		flow.append(stack)
+	else:
+		flow.append(c)
+		d.append(stack)
+		stack = c
+		q += 1
+
+with open('LZW_Decoded.txt', 'w') as F:
+	F.write(''.join(flow))
---
+++ LZW/LZW_encode.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+with open('LZW_toEncode.txt', 'r') as f:
+	s = f.read()
+# print('File contains:\n-----\n{}\n-----'.format(s))
+d = [chr(i) for i in range(256)]
+q = 256
+
+stack = ''
+flow = []
+
+for c in s:
+	stack += c
+	if stack not in d:
+		flow.append(d.index(stack[:-1]))
+		d.append(stack)
+		stack = c
+		q += 1
+
+if stack:
+	flow.append(d.index(stack))
+
+with open('LZW_Encoded.txt', 'w') as F:
+	F.write('\n'.join(map(str, flow)))
