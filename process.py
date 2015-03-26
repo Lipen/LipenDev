7fdafe65841df07adfacf64c232ea033fac4ca17
Lipen <lipen00@gmail.com>
2015-03-26 20:36:42 +0300

    Initial

---
+++ process.py
@@ -0,0 +1,86 @@
+import os
+sys = os.system
+
+FLAG_clear = ['/c', '-c']
+FLAG_window = ['/w', '-w']
+FLAG_exit = ['/e', '-e']
+
+def main():
+	print('List of existing <*.c++> files:')
+	files = []
+	counter = 0
+	for file in os.listdir():
+		if file[-4:] == '.c++':
+			counter += 1
+			files.append(file)
+			print('{:->3d}) {}'.format(counter, file[:-4]))
+
+	name = ''
+	flags = []
+	command, *ex = input('Enter your <command> [<name>] [<*flags>]: ').split()
+	if len(ex):
+		name = ex[0]
+		flags = list(ex[1:])
+		try:
+			name = files[int(name)-1]
+		except:
+			if name[0] == '#':
+				try:
+					fileid = int(name[1:])
+					name = files[fileid - 1]
+				except:
+					pass
+	else:
+		flags = list(ex)
+
+	if command=='open':
+		if len(list(set(FLAG_clear).intersection(set(flags))))>0:
+			sys('cls')
+		if len(list(set(FLAG_window).intersection(set(flags))))>0:
+			sys('start {}'.format(name))
+		else:
+			sys('{}'.format(name))
+	elif command=='compile':
+		if len(list(set(FLAG_clear).intersection(set(flags))))>0:
+			sys('cls')
+		print('Compiling...')
+		err = sys('g++ {} -o {}.exe'.format(name, name[:-4]))
+		if err:
+			print('Error during compiling. <{}>'.format(err))
+		else:
+			print('Compiled succesfully.')
+	elif command=='run':
+		if len(list(set(FLAG_clear).intersection(set(flags))))>0:
+				sys('cls')
+		print('Compiling...')
+		err = sys('g++ {} -o {}.exe'.format(name, name[:-4]))
+		if err:
+			print('Error during compiling. <{}>'.format(err))
+		else:
+			print('Compiled succesfully. Starting:\n'+'-'*31)
+			if len(list(set(FLAG_window).intersection(set(flags))))>0:
+				err2 = sys('start {}.exe'.format(name[:-4]))
+			else:
+				err2 = sys('{}.exe'.format(name[:-4]))
+			if err2:
+				print('-'*30+'\nError during execution. <{}>'.format(err2))
+			else:
+				print('-'*17+'\nDone succesfully.')
+	elif command=='list':
+		if name!='':
+			if len(list(set(FLAG_clear).intersection(set(flags))))>0:
+				sys('cls')
+			print('List of existing <*.{}> files:'.format(name))
+			l = len(name)
+			for file in os.listdir():
+				if file[-l:]==name:
+					print('{:>20}'.format(file[:-l-1]))
+		else:
+			print('List of all existing files:')
+			for file in os.listdir():
+				print('{:>20}'.format(file))
+
+	if len(list(set(FLAG_exit).intersection(set(flags))))==0:
+		input('-'*25+'\nEnd. Press enter to exit: ')
+
+main()
---
+++ run c++.py
@@ -0,0 +1,39 @@
+import os
+sys = os.system
+
+def main():
+    print('List of existing avaliable files:')
+    files = []
+    counter = 0
+    for file in os.listdir():
+        if file[-4:] == '.c++':
+            counter += 1
+            files.append(file[:-4])
+            print('{:->3d}) {}'.format(counter, file[:-4]))
+
+    name = ''
+    while len(name)==0:
+        name = input('Enter file <name> or <number> or #<number>: ')
+    try:
+        name = files[int(name)-1]
+    except:
+        if name[0] == '#':
+            try:
+                fileid = int(name[1:])
+                name = files[fileid - 1]
+            except:
+                pass
+
+    err = sys('g++ {0}.c++ -o {0}.exe'.format(name))
+    if err:
+        print('Error during compiling. <{}>'.format(err))    
+        input('\nType enter to exit: ')
+    else:
+        print('Compiled succesfully. Starting:\n'+'-'*30)
+        err2 = sys('{}.exe'.format(name))
+        if err2:
+            input('-'*30+'\nError during execution. <{}>'.format(err2))
+        else:
+            input('-'*30+'\nDone.\nType enter to exit: ')
+
+main()
