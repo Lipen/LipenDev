308aa55196c00678b742e853cb8e3291e99249f2
Lipen <lipen00@gmail.com>
2015-03-26 23:04:34 +0300

    Done

--- LZW/LZW_encode.cpp
+++ LZW/LZW_encode.cpp
@@ -2,11 +2,11 @@
 #include <fstream>
 #include <vector>
 #include <set>
+#include <map>
 #include <algorithm>
 #include <string>
 
 #define VI vector<int>
-#define VC vector<char>
 #define VVI vector<VI>
 #define FOR(i, n) for(int i = 0; i < (n); i++)
 #define FORS(i, si, n) for(int i = (si); i < (n); i++)
@@ -20,44 +20,43 @@ int n,m,x,a,b,c,d;
 
 int main()
 {
-	ifstream f("LZW_toEncode.txt");
-	VC s;
-	char c;
+	ifstream fi("LZW_toEncode.txt");
 
-	if (f.is_open()) {
-		while (f.get(c)) {
-			s.pb(c);
-			cout << c;
+	if (fi.is_open()) {
+		vector<string> flow;
+		char c;
+		string stack;
+		string newstack;
+		map<string, int> d;
+		for (int i=0; i<256; ++i) {
+			d[string(1, i)] = i;
 		}
-		f.close();
+		int last = 256;
+
+		while (fi.get(c)) {
+			newstack = stack + c;
+			if (d.find(newstack) == d.end()) {
+				flow.pb(stack);
+				d[newstack] = last++;
+				newstack = c;
+			}
+			stack = newstack;
+			// cout << "Stack: " << stack << endl;
+		}
+		flow.pb(stack); // BEWARE! :O
+
+		ofstream fo("LZW_Encoded.txt");
+		if (fo.is_open()) {
+			//
+			for (auto item : flow) {
+				fo << d[item] << endl;
+			}
+		} else {
+			cout << "Unable to open output file :C" << endl;
+		}
+		fi.close();
 	} else {
-		cout << "Unable to open file" << endl;
+		cout << "Unable to open input file :c" << endl;
 	}
 	return 0;
 }
-
-// int main()
-// {
-// setlocale(LC_ALL, "Russian");
-// ifstream in("C:\\Users\\user\\Desktop\\input.txt");
-// ofstream out("C:\\Users\\user\\Desktop\\output.txt");
-// if (in.is_open())
-// {
-// string sim;
-// //для считывания из файла пробелов (1 строчка)
-// sim.assign((istreambuf_iterator<char>(in.rdbuf())), istreambuf_iterator<char>());
-// cout « sim « endl;
-// //для опрелелния количества символов в кодируемом тексте (2 строчки)
-// basic_string <char>::size_type len;
-// len = sim.length();
-// cout « len « endl;
-// for (int i = 0; i < len; i++){
-// cout « sim[i] « " ";
-// }
-// system("pause");
-// in.close();
-// out.close();
-// }
-// else
-// cout « "File cannot be opened!" « endl;
-// }
