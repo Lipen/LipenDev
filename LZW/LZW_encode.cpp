bca2d4f21fc6724f1bd334efd38621426cfe7d9f
Lipen <lipen00@gmail.com>
2015-03-27 18:41:33 +0300

    Instant writing, no temporary storing vector

--- LZW/LZW_encode.cpp
+++ LZW/LZW_encode.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <map>
 #include <string>
-
-#define pb push_back
 
 using namespace std;
 
@@ -13,38 +10,28 @@ int main()
 	ifstream fi("LZW_toEncode.txt");
 
 	if (fi.is_open()) {
-		vector<string> flow;
-		char c;
-		string stack;
-		string newstack;
-		map<string, int> d;
-		for (int i=0; i<256; ++i) {
-			d[string(1, i)] = i;
-		}
-		int last = 256;
-
-		while (fi.get(c)) {
-			newstack = stack + c;
-			if (d.find(newstack) == d.end()) {
-				flow.pb(stack);
-				d[newstack] = last++;
-				newstack = c;
-			}
-			stack = newstack;
-		}
-		flow.pb(stack); // BEWARE! :O
-
 		ofstream fo("LZW_Encoded.txt");
+
 		if (fo.is_open()) {
-			//C++11:
-			// for (auto item : flow) {
-			// 	fo << d[item] << endl;
-			// }
-			//C++98:
-			vector<string>::iterator item = flow.begin();
-			for (; item != flow.end(); ++item) {
-				fo << d[*item] << endl;
+			char c;
+			string stack;
+			string newstack;
+			map<string, int> d;
+			for (int i=0; i<256; ++i) {
+				d[string(1, i)] = i;
 			}
+			int last = 256;
+
+			while (fi.get(c)) {
+				newstack = stack + c;
+				if (d.find(newstack) == d.end()) {
+					fo << d[stack] << " ";
+					d[newstack] = last++;
+					newstack = c;
+				}
+				stack = newstack;
+			}
+			fo << d[stack]; // BEWARE! :O
 		} else {
 			cout << "Unable to open output file :C" << endl;
 		}
