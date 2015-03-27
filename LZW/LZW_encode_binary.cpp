945e06290c7e7abe8b8d1df4cebc38412e9687a2
Lipen <lipen00@gmail.com>
2015-03-27 22:55:26 +0300

    Binary LZW encoder CPP

---
+++ LZW/LZW_encode_binary.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <string>
+#include <cmath>
+
+#define pb push_back
+
+using namespace std;
+
+string convert(long long n, long long size) {
+	string s;
+	for (int i=size-1; i>=0; --i) {
+		long long p = pow(2, i);
+		s += (n / p)?"1":"0";
+		n %= p;
+	}
+	return s;
+}
+
+int main()
+{
+	ifstream fi("LZW_toEncode.txt");
+
+	if (fi.is_open()) {
+		vector<string> flow;
+		char c;
+		string stack;
+		string newstack;
+		map<string, int> d;
+		for (int i=0; i<256; ++i) {
+			d[string(1, i)] = i;
+		}
+		int last = 256;
+
+		while (fi.get(c)) {
+			newstack = stack + c;
+			if (d.find(newstack) == d.end()) {
+				flow.pb(convert(d[stack], (long long)log2(last)+1));
+				d[newstack] = last++;
+				newstack = c;
+			}
+			stack = newstack;
+		}
+		flow.pb(convert(d[stack], (long long)log2(last)+1));
+
+		ofstream fo("LZW_Encoded.txt", ios::binary);
+
+		if (fo.is_open()) {
+			vector<string>::iterator item = flow.begin();
+			for (; item != flow.end(); ++item) {
+				fo << *item;
+			}
+			fo.close();
+		} else {
+			cout << "Unable to open output file :C" << endl;
+		}
+		fi.close();
+	} else {
+		cout << "Unable to open input file :c" << endl;
+	}
+	return 0;
+}
