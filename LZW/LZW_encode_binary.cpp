2eaba5154bee73667e2df2b86bd6b92433345f53
Lipen <lipen00@gmail.com>
2015-03-28 19:07:50 +0300

    minor fixes

--- LZW/LZW_encode_binary.cpp
+++ LZW/LZW_encode_binary.cpp
@@ -2,14 +2,14 @@
 #include <fstream>
 #include <vector>
 #include <map>
-#include <string>
+#include <cstring>
 #include <cmath>
 
 #define pb push_back
 
 using namespace std;
 
-string convert2bits(long long n, long long size) {
+string convert(long long n, long long size) {
 	string s;
 	for (int i=size-1; i>=0; --i) {
 		long long p = pow(2, i);
@@ -17,15 +17,6 @@ string convert2bits(long long n, long long size) {
 		n %= p;
 	}
 	return s;
-}
-
-unsigned char convert2byte(string s) {
-	int size = s.length();
-	unsigned char b = 0;
-	for (int i=size; i>0; --i) {
-		b += (int)pow(2, i-1);
-	}
-	return b;
 }
 
 int main()
@@ -46,13 +37,13 @@ int main()
 		while (fi.get(c)) {
 			newstack = stack + c;
 			if (d.find(newstack) == d.end()) {
-				flow += convert2bits(d[stack], (long long)log2(last)+1);
+				flow += convert(d[stack], (long long)log2(last)+1);
 				d[newstack] = last++;
 				newstack = c;
 			}
 			stack = newstack;
 		}
-		flow += convert2bits(d[stack], (long long)log2(last)+1);
+		flow += convert(d[stack], (long long)log2(last)+1);
 
 		ofstream fo("LZW_Encoded.txt", ios::binary);
 
