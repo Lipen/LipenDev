3ca67e87abb04f306a79cdaae29b92275e1aef1a
Lipen <lipen00@gmail.com>
2015-03-29 15:32:25 +0300

    Major decoder changes. Stable works now

--- LZW/LZW_decode_binary_bits.cpp
+++ LZW/LZW_decode_binary_bits.cpp
@@ -11,19 +11,17 @@ using namespace std;
 
 string convert2bits(unsigned char c) {
 	string s;
-	// cout << "convert2bits("<<c;
 	for (int i=0; i<8; ++i) {
 		int p = pow(2, 7-i);
-		s += (c / p)?"1":"0";
+		s += (c/p)?"1":"0";
 		c %= p;
 	}
-	// cout << "): Returning " << s << endl;
 	return s;
 }
 
-long long convert2int(string buf) {
-	long long size = buf.length();
-	long long x = 0;
+int convert2int(string buf) {
+	int size = buf.length();
+	int x = 0;
 	for (int i=0; i<size; ++i) {
 		if (buf[size-i-1]=='1') {
 			x += pow(2, i);
@@ -34,69 +32,59 @@ long long convert2int(string buf) {
 
 int main()
 {
-	ifstream fi("LZW_Encoded.txt", ios::binary|ios::in);
+	ifstream fi("LZW_Encoded.txt", ios::binary);
 
 	if (fi) {
-		vector<string> flow;
 		fi.seekg(0, fi.end);
-		int fileSize = fi.tellg();
+		int fileSize = fi.tellg() * 8; //bits
 		fi.seekg(0, fi.beg);
-		cout << "fileSize = " << fileSize << endl;
-		long long k;
-		string c;
-		string stack;
-		string newstack;
-		map<string, int> d;
+		map<int, string> d;
 		for (int i=0; i<256; ++i) {
-			d[string(1, i)] = i;
+			d[i] = string(1, i);
 		}
 		int last = 256;
 		int len = 9; //(int)log2(last+1)+1
 
 		char t;
 		string data;
-		data.reserve(8*fileSize);
+		data.reserve(fileSize);
 		while (fi.get(t)) {
 			data += convert2bits(t);
 		}
-		// cout << data << endl;
-		// cout << "data.len = " << data.length() << endl;
 
-		for (int i=0; i<fileSize*8; i+=len) {
+		int k = convert2int(data.substr(0, len));
+		string stack(1, k); //char group builder
+		string flow = stack; //result
+		string c; //current char/chargroup
+
+		for (int i=len; i<fileSize; i+=len) {
+			len = (int)log2(last+1)+1; //force(!) pre(!) ceil
 			string z = data.substr(i, len);
-			if (z.length() < len) break;
+
+			//Ignore trailing bits == last-byte-building-(zeros):
+			if (z.length() < len) {
+				cout << "Ignoring trailing " << z << endl;
+				break;
+			}
 			k = convert2int(z);
-			// cout << "substr = "<<data.substr(i, (int)log2(last+1)+1)<<"   k = " << k << " last="<<last<< endl;
-			map<string, int>::iterator it = d.begin();
-			for (; it != d.end(); ++it) {
-				if (it->second == k) {
-					c = it->first;
-					break;
-				}
-			}
-			stack += c;
-			if (d.find(stack) != d.end()) {
-				flow.pb(stack);
+
+			if (d.count(k)) {
+				c = d[k];
+			} else if (k==last) {
+				c = stack + stack[0];
 			} else {
-				flow.pb(c);
-				d[stack] = last++;
-				stack = c;
+				cout << "Bad encoded k = " << k << " at " << i << "!" <<endl;
 			}
-			len = (int)log2(last+1)+1;
+			flow += c;
+			d[last++] = stack+c[0];
+			stack = c;
 		}
 
 		ofstream fo("LZW_Decoded.txt");
 
 		if (fo) {
-			// C++11:
-			// for (auto item : flow) {
-			// 	fo << item;
-			// }
-			//C++98:
-			vector<string>::iterator item = flow.begin();
-			for (; item != flow.end(); ++item) {
-				fo << *item;
-			}
+			fo << flow;
+			fo.close();
 		} else {
 			cout << "Unable to open output file :C" << endl;
 		}
