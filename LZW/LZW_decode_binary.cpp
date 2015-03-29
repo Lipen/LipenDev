f1d3bfa06742ef86ceef115d42e1fb23c6c035e1
Lipen <lipen00@gmail.com>
2015-03-29 19:40:49 +0300

    Rename

--- LZW/LZW_decode_binary.cpp
+++ LZW/LZW_decode_binary.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <map>
-#include <cstring>
+#include <string>
 #include <cmath>
 
 #define pb push_back
 
 using namespace std;
 
-long long convert(char * buf) {
-	long long size = strlen(buf);
-	long long x = 0;
+string convert2bits(unsigned char c) {
+	string s;
+	for (int i=0; i<8; ++i) {
+		int p = pow(2, 7-i);
+		s += (c/p)?"1":"0";
+		c %= p;
+	}
+	return s;
+}
+
+int convert2int(string buf) {
+	int size = buf.length();
+	int x = 0;
 	for (int i=0; i<size; ++i) {
 		if (buf[size-i-1]=='1') {
 			x += pow(2, i);
@@ -22,50 +31,59 @@ long long convert(char * buf) {
 
 int main()
 {
-	ifstream fi("LZW_Encoded.txt");
+	ifstream fi("LZW_Encoded.txt", ios::binary);
 
-	if (fi.is_open()) {
-		vector<string> flow;
-		char * buf = new char[1000];
-		long long k;
-		string c;
-		string stack;
-		string newstack;
-		map<string, int> d;
+	if (fi) {
+		fi.seekg(0, fi.end);
+		int fileSize = fi.tellg() * 8; //bits
+		fi.seekg(0, fi.beg);
+		map<int, string> d;
 		for (int i=0; i<256; ++i) {
-			d[string(1, i)] = i;
+			d[i] = string(1, i);
 		}
 		int last = 256;
+		int len = 9; //(int)log2(last+1)+1
 
-		while (fi.read(buf, (long long)log2(last+1)+1)) {
-			k = convert(buf);
-			map<string, int>::iterator it = d.begin();
-			for (; it != d.end(); ++it )
-				if (it->second == k) {
-					c = it->first;
-					break;
-				}
-			stack += c;
-			if (d.find(stack) != d.end()) {
-				flow.pb(stack);
-			} else {
-				flow.pb(c);
-				d[stack] = last++;
-				stack = c;
-			}
+		char t;
+		string data;
+		data.reserve(fileSize);
+		while (fi.get(t)) {
+			data += convert2bits(t);
 		}
 
-		ofstream fo("LZW_Decoded.txt");
-		if (fo.is_open()) {
-			//C++11:
-			// for (auto item : flow) {
-			// 	fo << item;
-			// }
-			//C++98:
-			vector<string>::iterator item = flow.begin();
-			for (; item != flow.end(); ++item) {
-				fo << *item;
+		int k = convert2int(data.substr(0, len));
+		string stack(1, k); //char group builder
+		string flow = stack; //result
+		string c; //current char/chargroup
+
+		for (int i=len; i<fileSize; i+=len) {
+			len = (int)log2(last+1)+1; //force(!) pre(!) ceil
+			string z = data.substr(i, len);
+
+			//Ignore trailing bits == last-byte-building-(zeros):
+			if (z.length() < len) {
+				cout << "Ignoring trailing " << z << endl;
+				break;
 			}
+			k = convert2int(z);
+
+			if (d.count(k)) {
+				c = d[k];
+			} else if (k==last) {
+				c = stack + stack[0];
+			} else {
+				cout << "Bad encoded k = " << k << " at " << i << "!" <<endl;
+			}
+			flow += c;
+			d[last++] = stack+c[0];
+			stack = c;
+		}
+
+		ofstream fo("LZW_Decoded.txt", ios::binary);
+
+		if (fo) {
+			fo << flow;
+			fo.close();
 		} else {
 			cout << "Unable to open output file :C" << endl;
 		}
