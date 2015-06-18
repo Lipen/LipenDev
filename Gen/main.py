e8738fc506732811b65ac98f4c3de7fbd719a78c
Lipen <lipen00@gmail.com>
2015-06-18 12:41:48 +0500

    Now gene <R> increasing feeding rate

--- Gen/main.py
+++ Gen/main.py
@@ -1,5 +1,6 @@
 import random
 import re
+import math
 
 
 # Some species:
@@ -8,6 +9,8 @@ import re
 class Cell:
 	seq = ''
 	genes = ''
+	food = 0
+	feeding_rate = 1.
 
 	def __init__(self, seq):
 		self.seq = seq
@@ -19,13 +22,39 @@ class Cell:
 		t = re.match('.*?(ATG)+(?P<genes>(...)*?)((TAG)|(TAA)|(TGA)).*', self.seq)
 		if t:
 			self.genes = convertToProteins(t.group('genes'))
+
+			# There goes genes' functions:
+			self.feeding_rate = max(.1, 1. + 0.5*math.sqrt(self.genes.count('R')) - 1.*self.genes.count('E'))
+
+			##
 			return True
 		else:
 			return False
 
+	def feed(self, k=1):
+		self.food += k*self.feeding_rate
+
 	def mutate(self):
 		t = random.randint(0, len(self.seq)-1)
 		self.seq = self.seq[:t] + {'A': 'GCT', 'G': 'ACT', 'T': 'AGC', 'C': 'ATG'}[self.seq[t]][random.randint(0, 2)] + self.seq[t+1:]
+
+	def produce(self):
+		F = []
+		k = int(self.food)
+		lim = 10
+		ex = 5
+		if k > lim:
+			k = lim + (k-lim)//ex
+			F.append(self)  # Also reproduces itself
+
+		for i in range(k):
+			t = Cell(self.seq)
+			t.mutate()
+			if t.test():
+				F.append(t)
+
+		self.food = 0
+		return F
 
 
 def initial_random_cell(n=20):
@@ -47,16 +76,24 @@ def convertToProteins(s):
 	return ''.join(d[s[i*3:(i+1)*3]] for i in range(len(s)//3))
 
 
-def turn(A):
+def turn(A, i):
 	F = []
 
+	food = 50 + i//2
+	while food > 0:
+		a = random.choice(A)
+		a.feed(1)
+		food -= 1
+
 	for i in range(len(A)):
-		q = A[i].seq
-		for k in range(5):
-			t = Cell(q)
-			t.mutate()
-			if t.test():
-				F.append(t)
+		f = A[i].produce()
+		F.extend(f)
+		# q = A[i].seq
+		# for k in range(5):
+		# 	t = Cell(q)
+		# 	t.mutate()
+		# 	if t.test():
+		# 		F.append(t)
 
 	return F
 
@@ -66,20 +103,20 @@ def main():
 	n = 1
 
 	while len(F) < n:
-		f = initial_random_cell()
+		f = initial_random_cell(50)
 		if f.test():
 			F.append(f)
 			print('Born #{}: {} :: {}'.format(len(F), f.seq, f.getGenes()))
 
-	print('Initial generation:')
+	print('Initial generation: ({} species)'.format(len(F)))
 	for i in range(len(F)):
 		print('#{: <2}: {} :: {}'.format(i+1, F[i].seq, F[i].getGenes()))
 	print('-------------------')
 
-	for _ in range(5):  # skip turns
-		F = turn(F)
+	for i in range(100):  # skip turns
+		F = turn(F, i)
 
-	print('New generation:')
+	print('New generation: ({} species)'.format(len(F)))
 	for i in range(len(F)):
 		print('#{: <2}: {} :: {}'.format(i+1, F[i].seq, F[i].getGenes()))
 	print('-------------------')
