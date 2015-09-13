887218ccda18ff1700d84836a57fe96e569d044d
Lipen <lipen00@gmail.com>
2015-09-13 15:35:15 +0300

    Added hasPath method + changed DFS recursive calls + adjmatrix implemented

--- GraphLib/graphlib_main.cpp
+++ GraphLib/graphlib_main.cpp
@@ -48,6 +48,13 @@ class Vertex {
 	double getWeight() const {
 		return weight;
 	}
+
+	void resetData(bool reset_only_visited = false) {
+		visited = false;
+		if (!reset_only_visited) {
+			weight = 0.0;
+		}
+	}
 };
 
 
@@ -79,6 +86,21 @@ class Edge {
 	void applyAdjList(std::map<string, std::set<Vertex*>>& adj_list) {
 		adj_list[start->toString()].insert(finish);
 	}
+
+	void applyAdjMatrix(std::map<string, std::map<string, int>>& adj_matrix) {
+		adj_matrix[start->toString()][finish->toString()] = 1;
+	}
+
+	double getWeight() const {
+		return weight;
+	}
+
+	void resetData(bool reset_only_visited = false) {
+		visited = false;
+		if (!reset_only_visited) {
+			weight = 0.0;
+		}
+	}
 };
 
 
@@ -86,13 +108,18 @@ class Graph {
 	std::set<Vertex*> vertices;
 	std::set<Edge*> edges;
 	std::map<string, std::set<Vertex*>> adj_list;
+	std::map<string, std::map<string, int>> adj_matrix;
 
  public:
 	Graph() {}
 
 	Vertex* addVertex(Vertex* v) {
-		std::set<Vertex*> adj_to;  // tmp
-		adj_list[v->toString()] = adj_to;
+		adj_list[v->toString()] = std::set<Vertex*>();
+		adj_matrix[v->toString()] = std::map<string, int>();
+		for (Vertex* u : vertices) {
+			adj_matrix[v->toString()][u->toString()] = 0;
+			adj_matrix[u->toString()][v->toString()] = 0;
+		}
 		vertices.insert(v);
 		return v;
 	}
@@ -103,6 +130,7 @@ class Graph {
 
 	Edge* addEdge(Edge* e) {
 		e->applyAdjList(adj_list);
+		e->applyAdjMatrix(adj_matrix);
 		edges.insert(e);
 		return e;
 	}
@@ -161,12 +189,6 @@ class Graph {
 		return vertices.find(v) != vertices.end();
 	}
 	bool containsVertex(string v_name) {
-		// for (Vertex* v : vertices) {
-		// 	if (v->toString() == v_name) {
-		// 		return true;
-		// 	}
-		// }
-		// return false;
 		return getVertex(v_name) != nullptr;
 	}
 
@@ -174,41 +196,49 @@ class Graph {
 		return edges.find(e) != edges.end();
 	}
 	bool containsEdge(Vertex* v1, Vertex* v2) {
-		// for (Edge* e : edges) {
-		// 	if (e->isEqual(v1, v2)) {
-		// 		return true;
-		// 	}
-		// }
-		// return false;
 		return getEdge(v1, v2) != nullptr;
 	}
 	bool containsEdge(string v1_name, string v2_name) {
-		// for (Edge* e : edges) {
-		// 	if (e->isEqual(v1_name, v2_name)) {
-		// 		return true;
-		// 	}
-		// }
-		// return false;
 		return getEdge(v1_name, v2_name) != nullptr;
 	}
 
-	Vertex* DFS_explore(Vertex* v, string finish_name) {
+	void resetData(bool reset_only_visited = false) {
+		for (Vertex* v : vertices) {
+			v->resetData(reset_only_visited);
+		}
+		for (Edge* e : edges) {
+			e->resetData(reset_only_visited);
+		}
+	}
+
+	Vertex* DFS(Vertex* v, Vertex* finish) {
 		v->visit();
-		if (v->toString() == finish_name) {
+
+		if (v == finish) {
 			return v;
 		}
 
 		for (Vertex* u : adj_list[v->toString()]) {
 			if (!u->isVisited()) {
-				return DFS_explore(u, finish_name);
+				return DFS(u, finish);
 			}
 		}
 
 		return nullptr;
 	}
 
-	Vertex* DFS(Vertex* start, string finish_name) {
-		return DFS_explore(start, finish_name);
+	bool hasPath(Vertex* start, Vertex* finish) {
+		resetData(true);
+		return DFS(start, finish) != nullptr;
+	}
+	bool hasPath(Vertex* start, string finish_name) {
+		return hasPath(start, getVertex(finish_name));
+	}
+	bool hasPath(string start_name, Vertex* finish) {
+		return hasPath(getVertex(start_name), finish);
+	}
+	bool hasPath(string start_name, string finish_name) {
+		return hasPath(getVertex(start_name), getVertex(finish_name));
 	}
 };
 
@@ -247,12 +277,17 @@ int main() {
 
 	cout << sizeof(g) << std::endl;
 
+	// T T F T F
 	cout << (g.containsEdge("1", "2")?"dbg:true":"dbg:false") << std::endl;
 	cout << (g.containsEdge("2", "4")?"dbg:true":"dbg:false") << std::endl;
+	cout << (g.containsEdge("1", "3")?"dbg:true":"dbg:false") << std::endl;
 	cout << (g.containsVertex("9")?"dbg:true":"dbg:false") << std::endl;
 	cout << (g.containsVertex("42")?"dbg:true":"dbg:false") << std::endl;
 
-	Vertex* v7 = g.DFS(v1, "7");
-
+	Vertex* v7 = g.DFS(v1, g.getVertex("7"));
 	cout << "Found v7: " << *v7 << std::endl;
+
+	// F T
+	cout << "hasPath(2 -> 9) = " << (g.hasPath("2", "9")?"yes":"no") << '\n';
+	cout << "hasPath(1 -> 7) = " << (g.hasPath("1", "7")?"yes":"no") << '\n';
 }
