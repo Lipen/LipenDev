77628aeb0db6b56cfbddfc2ab14c1ce2ac2faf57
Lipen <lipen00@gmail.com>
2015-11-29 19:01:55 +0300

    k-th order statistic minor update

--- Cormen/select-k-th-element.cpp
+++ Cormen/select-k-th-element.cpp
@@ -1,57 +1,74 @@
-#include <iostream>  	// cout
-#include <utility>  	// swap
-#include <random>  		// default_random_engine, uniform_int_distribution
-#include <algorithm>  	// max, shuffle
-
-#include <ctime>  		// clock
+#include <iostream>   // cout
+#include <utility>    // swap
+#include <cstdlib>    // rand
+#include <ctime>      // time
+#include <algorithm>  // max, shuffle
 
 using std::cout;
 using std::endl;
 
-static std::default_random_engine RandomEngine;
-
 
 template<typename T, size_t N>
-size_t size(T (&)[N]) { return N; }
+constexpr size_t size(T (&)[N]) { return N; }
 
 template<typename T, size_t N>
 void show(T (&A)[N]) {
-	for (int i = 0; i < static_cast<int>(size(A)); i++) {
+	for (size_t i = 0; i < N; i++) {
 		cout << A[i] << " ";
 	}
 	cout << endl;
 }
 
+template<typename T>
+constexpr T random(T xmin, T xmax) {
+	return rand() % (xmax - xmin + 1) + xmin;
+}
+
+
+// Cormen`s.
+// template<typename T, size_t N>
+// size_t partition(T (&A)[N], size_t p = 0, size_t r = N-1) {
+// 	T x = A[p];
+// 	size_t i = p;
+// 	size_t j = r;
+
+// 	while (true) {
+// 		while (A[j] > x) { --j; }
+// 		while (A[i] < x) { ++i; }
+
+// 		if (i < j) {
+// 			std::swap(A[i], A[j]);
+// 		} else {
+// 			return j;
+// 		}
+// 	}
+// }
 
 template<typename T, size_t N>
-size_t partition(T (&A)[N], size_t p = 0, size_t r = N-1) {
-	T x = A[p];
-	size_t i = p;
-	size_t j = r;
+size_t partition(T (&A)[N], size_t l = 0, size_t r = N-1) {
+	T x = A[l];
+	size_t j = l;
 
-	while (true) {
-		while (A[j] > x) { --j; }
-		while (A[i] < x) { ++i; }
-
-		if (i < j) {
-			std::swap(A[i], A[j]);
-		} else {
-			return j;
+	for (size_t i = l+1; i <= r; ++i) {
+		if (A[i] < x) {
+			++j;
+			std::swap(A[j], A[i]);
 		}
 	}
+
+	std::swap(A[l], A[j]);
+	return j;
 }
 
 template<typename T, size_t N>
 size_t partition_randomized(T (&A)[N], size_t p = 0, size_t r = N-1) {
-	std::uniform_int_distribution<size_t> generator(p, r);
-	std::swap(A[p], A[generator(RandomEngine)]);
+	std::swap(A[p], A[random(p, r)]);
 	return partition(A, p, r);
 }
 
 
 template<typename T, size_t N>
-size_t select_simple(T (&A)[N], size_t k = 0, size_t p = 0, size_t r = N-1) {
-	// k = 0..N-1
+size_t select_simple(T (&A)[N], size_t k = 0) {  // k = 0..N-1
 	for (size_t i = 0; i <= k; ++i) {
 		size_t min_index = i;
 		T min_value = A[i];
@@ -70,8 +87,7 @@ size_t select_simple(T (&A)[N], size_t k = 0, size_t p = 0, size_t r = N-1) {
 }
 
 template<typename T, size_t N>
-size_t select_randomized(T (&A)[N], size_t i = 1, size_t p = 0, size_t r = N-1) {
-	// i = 1..N
+size_t select_randomized(T (&A)[N], size_t i = 1, size_t p = 0, size_t r = N-1) {  // Cormen`s
 	if (p == r) {
 		return A[p];
 	}
@@ -87,17 +103,47 @@ size_t select_randomized(T (&A)[N], size_t i = 1, size_t p = 0, size_t r = N-1) 
 }
 
 
-int main() {
+int main_wrapped() {
+ { cout << "Select-Randomized tests:" << endl;
 	int A[] {32, 71, 85, 75, 28, 3, 29, 82, 55, 16, 57, 92, 79, 98, 1, 13, 11, 56, 9, 31, 15, 2, 64, 14, 5, 27, 89, 22, 40, 18};  // 30
-	cout << "Initial:\n\t"; show(A);
+	cout << "Initial:\n\t";  show(A);
 
-	// for (int i = 1; i <= static_cast<int>(size(A)); ++i) {
-	// 	int s = select_randomized(A, i);
-	// 	cout << "Select-Randomized("<<i<<") >> " << s << endl;
-	// }
+	for (size_t i = 1; i <= size(A); ++i) {
+		int s = select_randomized(A, i);
+		cout << "Select-Randomized("<<i<<") >> " << s << endl;
+	}
 
-	for (int i = 0; i < static_cast<int>(size(A)); ++i) {
-		int s = select_simple(A, i);
+	cout << "Post:\n\t";  show(A);
+ }
+
+ { cout << "\nSelect-Simple tests:" << endl;
+	int A[] {32, 71, 85, 75, 28, 3, 29, 82, 55, 16, 57, 92, 79, 98, 1, 13, 11, 56, 9, 31, 15, 2, 64, 14, 5, 27, 89, 22, 40, 18};  // 30
+	cout << "Initial:\n\t";  show(A);
+
+	for (size_t i = 0; i < size(A); ++i) {
+		size_t s = select_simple(A, i);
 		cout << "Select-Simple("<<i<<") >> " << s << endl;
+	}
+
+	cout << "Post:\n\t";  show(A);
+ }
+	return 0;
+}
+
+int main() {
+	try {
+		int e = main_wrapped();
+		if (e == 0) {
+			cout << "\nOK" << endl;
+		}
+		else {
+			cout << "\nMain returned errcode = " << e << endl;
+		}
+	}
+	catch (const char* msg) {
+		cout << "\nException caught: " << msg << endl;
+	}
+	catch (...) {
+		cout << "\nSome exception caught." << endl;
 	}
 }
