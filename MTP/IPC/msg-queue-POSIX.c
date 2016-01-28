8af808c2e7df81ab4583ce1057af5ac38d21ffc7
Lipen <lipen00@gmail.com>
2016-01-28 22:19:38 +0300

    MTP: POSIX mqueue works

--- MTP/IPC/msg-queue-POSIX.c
+++ MTP/IPC/msg-queue-POSIX.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <mqueue.h>
@@ -6,18 +6,20 @@
 
 int main(int argc, char* argv[]) {
 	struct mq_attr attr;
-	attr.flags = 0;
-	attr.mq_maxmsg = 32;
-	attr.mq_msgsize = 512;
+	attr.mq_flags = 0;
+	attr.mq_maxmsg = 10;
+	attr.mq_msgsize = 8192;
 
 	mqd_t mq = mq_open("/test.mq", O_RDONLY | O_CREAT, 0666, &attr);
 
-	char buf[512];
-	int result = mq_recieve(mq, buf, 512, 0);
+	char buf[8192];
+	int result = mq_receive(mq, buf, 8192, 0);
 
-	int fd = open("message.txt", O_WRONLY | O_CREAT);
+	int fd = open("message.txt", O_WRONLY | O_CREAT, 0666);
 	write(fd, buf, result);
 	close(fd);
+
+	mq_unlink("/test.mq");
 
 	return 0;
 }
