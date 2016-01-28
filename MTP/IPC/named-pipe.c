d7aa7cd1a3617d994e725cef37ae546909d86ed6
Lipen <lipen00@gmail.com>
2016-01-28 15:21:41 +0300

    MTP: Named pipe example - final

--- MTP/IPC/named-pipe.c
+++ MTP/IPC/named-pipe.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int main(int argc, char* argv[]) {
-	int fifo_in = mkfifo("in.fifo", 0666);
-	int fifo_out = mkfifo("out.fifo", 0666);
+	mkfifo("in.fifo", 0666);
+	mkfifo("out.fifo", 0666);
+
+	int fifo_in = open("in.fifo", O_RDONLY | O_CREAT);
+	int fifo_out = open("out.fifo", O_WRONLY | O_CREAT);
 
 	char buf[512];
 	ssize_t r;
 
-	printf("Line 12...\n");
-
-	while ((r = read(fifo_in, buf, 512)) > 0) {
+	while ((r = read(fifo_in, buf, 10)) > 0) {
 		printf("Read %zu: %s\n", r, buf);
 		write(fifo_out, buf, r);
-		memset(buf, 0, 512);
 	}
-
-	if (r < 0) {
-		perror("Read -1");
-	}
-
-	printf("Closing...\n");
 
 	close(fifo_in);
 	close(fifo_out);
